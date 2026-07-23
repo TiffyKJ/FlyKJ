b9b18284873fc9718c4bb94d5ee834886d43bf3f
TiffyKJ <keyleblue@163.com>
2026-07-23 19:40:53 +0800

    Refactor structures in types.h for clarity

--- src/types.h
+++ src/types.h
@@ -1,28 +1,37 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+
 #include <stdint.h>
 #include <stdbool.h>
 
 
 /*
  * Raw Mode-S message
+ *
+ * ADS-B extended squitter:
+ *
  * 112 bits = 14 bytes
+ *
+ * This structure contains only
+ * raw received frame data.
  */
 typedef struct
 {
     uint8_t data[14];
 
-    // microseconds since receiver start
+    /*
+     * Microseconds since receiver start
+     */
     uint64_t timestamp;
 
-}
-ModeSMessage;
+} ModeSMessage;
+
 
 
 
 /*
- * Result of decoding one ADS-B message
+ * Decoded ADS-B message result
  */
 typedef struct
 {
@@ -30,58 +39,81 @@ typedef struct
 
 
     bool has_callsign;
+
     char callsign[9];
 
 
     bool has_position;
+
     double lat;
+
     double lon;
+
+    int altitude;
 
 
     bool has_velocity;
+
     float velocity;
+
     float heading;
 
 
-    int altitude;
-}
-AdsbDecodedData;
+} AdsbDecodedData;
 
 
 
 /*
  * CPR even/odd frame cache
+ *
+ * Used for global position decoding.
  */
 typedef struct
 {
 
     uint32_t even_lat;
+
     uint32_t even_lon;
 
+
     uint32_t odd_lat;
+
     uint32_t odd_lon;
 
 
     uint64_t even_time;
+
     uint64_t odd_time;
 
 
     bool has_even;
+
     bool has_odd;
-}
-CPRState;
+
+} CPRState;
+
 
 
 
 /*
- * Tracked aircraft
+ * Aircraft tracking object
+ *
+ * Stored in aircraft database.
  */
 typedef struct
 {
     uint32_t icao;
+
     AdsbDecodedData data;
+
     CPRState cpr;
+
+    /*
+     * Microseconds since receiver start
+     */
     uint64_t last_seen;
-}
-Aircraft;
+
+} Aircraft;
+
+
 #endif
