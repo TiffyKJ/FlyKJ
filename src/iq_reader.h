bb8dae5d4c7ad981a24fbece06e6def5c1589f10
TiffyKJ <keyleblue@163.com>
2026-07-23 19:15:28 +0800

    Fix formatting and syntax in iq_reader.h

--- src/iq_reader.h
+++ src/iq_reader.h
@@ -6,36 +6,39 @@
 #include <stddef.h>
 #include <stdio.h>
 
+
+/*
+ * Raw RTL-SDR IQ sample
+ *
+ * File format:
+ *
+ * byte0: I
+ * byte1: Q
+ */
+
+
 typedef struct
 {
     uint8_t i;
     uint8_t q;
-
-}
-IQSample;
+} IQSample;
 
 typedef struct
 {
-    FILE* file;
-
+    FILE *file;
     uint64_t samples_read;
-
-}
-IQReader;
+} IQReader;
 
 bool iq_reader_open(
-        IQReader* reader,
-        const char* filepath);
-
+        IQReader *reader,
+        const char *filepath);
 
 size_t iq_reader_read(
-        IQReader* reader,
-        IQSample* buffer,
+        IQReader *reader,
+        IQSample *buffer,
         size_t max_samples);
 
-
 void iq_reader_close(
-        IQReader* reader);
-
+        IQReader *reader);
 
 #endif
